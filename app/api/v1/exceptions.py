f2851860577832f5554341a6b2b94583813bbbe6
axlerya <alexander.ryabov02@mail.ru>
2026-01-21 17:07:15 +0500

    feat: implement error handling responses and define error response schema

---
+++ app/api/v1/exceptions.py
@@ -0,0 +1,9 @@
+from fastapi import HTTPException, status
+
+# GROUP_NOT_FOUND = HTTPException(
+#     status_code=status.HTTP_404_NOT_FOUND,
+#     detail={
+#         "error": "group_not_found",
+#         "message": "Группа не найдена.",
+#     },
+# )
---
+++ app/docs/responses.py
@@ -0,0 +1,29 @@
+from fastapi import status
+from app.schemas.error import ErrorResponse
+
+def response_error(status_code: int, error: str, message: str):
+    return {
+        status_code: {
+            "model": ErrorResponse,
+            "description": message,
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "default": {
+                            "summary": message,
+                            "value": {
+                                "error": error,
+                                "detail": message
+                            }
+                        }
+                    }
+                }
+            },
+        }
+    }
+
+# invalid_group_id_response = response_error(
+#     status.HTTP_422_UNPROCESSABLE_ENTITY,
+#     "invalid_group_id",
+#     "Передан некорректный идентификатор VK группы.",
+# )
